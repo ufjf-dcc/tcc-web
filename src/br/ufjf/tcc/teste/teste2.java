510e663157678b7263fbb38a2f8217b15373ffdc
fanuel <fanuelson@gmail.com>
2015-12-19 11:26:37 -0200

    Adicionado Atas usadas como template para geração de PDF.
    Issue: #106

---
+++ src/br/ufjf/tcc/teste/teste2.java
@@ -0,0 +1,14 @@
+package br.ufjf.tcc.teste;
+
+import br.ufjf.tcc.library.Encripita;
+
+public class teste2 {
+
+	public static void main(String[] args) {
+			Encripita e = new Encripita();
+			
+			System.out.println(e.encripta("fanu123"));
+
+	}
+
+}
