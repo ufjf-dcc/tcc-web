3e206b494170bad59f5a835de32e410112356a35
fanuel nunes <fanuelson@gmail.com>
2016-03-02 01:41:42 -0300

    Arquitetura inicial para envio de emails

--- src/br/ufjf/tcc/mail/Email.java
+++ src/br/ufjf/tcc/mail/Email.java
@@ -1,5 +1,44 @@
 package br.ufjf.tcc.mail;
 
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import br.ufjf.tcc.library.ConfHandler;
+
 public class Email {
+
+	private Session session;
+	private Message message;
+
+	public Email() {
+		if (session == null) {
+			session = Session.getInstance(EmailProperties.getPropriedades(), new Autenticacao());
+			session.setDebug(true);
+		}
+		message = new MimeMessage(session);
+	}
+
+	public void enviar(EmailBuilder builder) {
+		try {
+			message.setFrom(new InternetAddress(ConfHandler.getConf("MAIL.FROM"))); // Remetente
+
+			Address[] toUser = InternetAddress.parse(builder.getDestinatarios()); // Destinatário(s)
+
+			message.setRecipients(Message.RecipientType.TO, toUser);
+			message.setSubject("Enviando email com JavaMail"); // Assunto
+			message.setText(builder.getMensagem());
+
+			Transport.send(message); // Método para enviar a mensagem criada
+			System.out.println("Email enviado com sucesso!!\n\n");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
--- src/br/ufjf/tcc/mail/EmailBuilder.java
+++ src/br/ufjf/tcc/mail/EmailBuilder.java
@@ -2,4 +2,31 @@ package br.ufjf.tcc.mail;
 
 public class EmailBuilder {
 
+	private final StringBuilder mensagem;
+	private final StringBuilder destinatarios;
+
+	public EmailBuilder() {
+		mensagem = new StringBuilder();
+		destinatarios = new StringBuilder();
+	}
+
+	public void appendDestinatario(String email) {
+		if (!destinatarios.toString().isEmpty()) {
+			destinatarios.append(", ");
+		}
+		destinatarios.append(email);
+	}
+
+	public void appendMensagem(String texto) {
+		mensagem.append(texto);
+	}
+
+	public String getDestinatarios() {
+		return this.destinatarios.toString();
+	}
+
+	public String getMensagem() {
+		return this.mensagem.toString();
+	}
+
 }
