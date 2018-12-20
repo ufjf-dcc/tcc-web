423143e490eebef1787cea3f479cddcb0cbd6242
André Caetano <andrecvidal@hotmail.com>
2018-12-20 09:36:07 -0200

    Mudanças nas configurações e novo IntegraAPI adicionado

--- src/br/ufjf/tcc/library/ConfHandler.java
+++ src/br/ufjf/tcc/library/ConfHandler.java
@@ -8,6 +8,10 @@ import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 import org.apache.commons.io.IOUtils;
 
 public class ConfHandler {
@@ -16,10 +20,14 @@ public class ConfHandler {
 
 	private ConfHandler() {
 		try {
-			//InputStream inputStream = new FileInputStream("/tcc-config/config.txt");
-			InputStream inputStream = new FileInputStream("/tcc-teste-config/config.txt");
+			Context env = (Context) new InitialContext().lookup("java:comp/env");
+			final String configPath = (String) env.lookup("config-path");
+			
+			//InputStream inputStream = new FileInputStream("/dcc-config/tcc/config.txt");
+			InputStream inputStream = new FileInputStream(configPath);
 	        String arquivo = IOUtils.toString(inputStream);
-			confs = new HashMap<String, String>();
+			
+	        confs = new HashMap<String, String>();
 			Pattern patternConf = Pattern.compile("^([A-Z]+\\.[A-Z]+) = (.*)$", Pattern.MULTILINE);
 			Matcher conf = patternConf.matcher(arquivo);
 			while (conf.find()) {
@@ -29,6 +37,9 @@ public class ConfHandler {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
--- src/br/ufjf/tcc/library/IntegraHandler.java
+++ src/br/ufjf/tcc/library/IntegraHandler.java
@@ -3,12 +3,11 @@ package br.ufjf.tcc.library;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.ufjf.ice.integra3.ws.login.interfaces.IWsLogin;
-import br.ufjf.ice.integra3.ws.login.interfaces.Profile;
-import br.ufjf.ice.integra3.ws.login.interfaces.WsException_Exception;
-import br.ufjf.ice.integra3.ws.login.interfaces.WsLoginResponse;
-import br.ufjf.ice.integra3.ws.login.interfaces.WsUserInfoResponse;
-import br.ufjf.ice.integra3.ws.login.service.WSLogin;
+import br.ufjf.ice.integra3.ws.login.IWsLogin;
+import br.ufjf.ice.integra3.ws.login.Profile;
+import br.ufjf.ice.integra3.ws.login.WsLoginResponse;
+import br.ufjf.ice.integra3.ws.login.WsUserInfoResponse;
+import br.ufjf.ice.integra3.ws.login.WSLogin;
 
 public class IntegraHandler {
 
@@ -19,11 +18,10 @@ public class IntegraHandler {
 		profiles = new ArrayList<String>();
 	}
 
-	public void doLogin(String login, String password) throws WsException_Exception {
+	public void doLogin(String login, String password) {
+		try {
 			IWsLogin integra = new WSLogin().getWsLoginServicePort();
-			WsLoginResponse user = integra.login(login,
-					password,
-					ConfHandler.getConf("INTEGRA.APPTOKEN"));
+			WsLoginResponse user = integra.login(login, password, ConfHandler.getConf("INTEGRA.APPTOKEN"));
 
 			infos = integra.getUserInformation(user.getToken()); // Pegando informações
 
@@ -32,6 +30,10 @@ public class IntegraHandler {
 			for (Profile profile : profilesList) { // Varrendo a lista de Profiles e pegando as Matriculas do usuário
 				profiles.add(profile.getMatricula());
 			}
+		}
+		catch(Exception ex) {
+			
+		}
 	}
 
 	public List<String> getProfiles() {
